5b21ffe1fd1e359774b8c06caf96c633ba7a14ff
VictorNivelo <104579002+VictorNivelo@users.noreply.github.com>
2022-12-21 13:01:51 -0500

    Enfermera

---
+++ src/Modelo/Enfermera.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author Victor
+ */
+public class Enfermera {
+    private String Tipo;
+    private String codigo;
+    private String rol;
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+    
+}
